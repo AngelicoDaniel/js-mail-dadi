e19e172bc425be83ea303e21a0fcd0d1a1b7578b
Daniel <angelicodaniel93@gmail.com>
2022-10-24 16:49:40 +0200

    rifatto ma stesso problema

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,23 @@
+const arrEmail = ['qwerty@gmail.com', 'pippo123@gmail.com', 'pluto456@gmail.com', 'paolo.rossi@gmail.com', 'wasd666@gmail.com']
+
+
 function login(){
 
 let emailValue = document.getElementById('email').value;
-console.log(emailValue)
 
-const arrEmail = ['qwerty@gmail.com', 'pippo123@gmail.com', 'pluto456@gmail.com', 'paolo.rossi@gmail.com', 'wasd666@gmail.com']
-console.log(arrEmail)
+
 
 for (let i = 0; i < arrEmail.length; i++) {
     console.log(arrEmail[i])
-    if (emailValue == arrEmail) {
-      console.log("corretto");
-    } else if (emailValue !== arrEmail) {
-      console.log("sbagliato");
-    }
-  }
 
+    if (emailValue == arrEmail[i]) {
+    console.log('corretto')
+
+    } else (emailValue !== arrEmail[i]) 
+    console.log('sbagliato');
+    }
 }
+
 
 
 
