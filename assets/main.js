fba45c00b8905d61086b6c02e8dc8649182077b2
Daniel <angelicodaniel93@gmail.com>
2022-10-24 16:11:53 +0200

    dovrebbe funzionare

--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,19 @@
+function login(){
+
+let emailValue = document.getElementById('email').value;
+console.log(emailValue)
+
 const arrEmail = ['qwerty@gmail.com', 'pippo123@gmail.com', 'pluto456@gmail.com', 'paolo.rossi@gmail.com', 'wasd666@gmail.com']
 console.log(arrEmail)
 
-const inputEmail = document.getElementById('email')
-
-
-function login(){
-    let emailValue = inputEmail.value;
-    console.log (emailValue)
-    
-    for (let i = 0; i <= arrEmail.length; i++){
-        console.log(arrEmail[i])
-
-    if (emailValue == arrEmail[i]) {
-        document.getElementById('check').innerHTML = `${'email corretta'}`
-        console.log('emailcorretta')
-    } else {
-        document.getElementById('check').innerHTML = `${'hai sbagliato'}`
-        console.log('sbagliato')
-    } 
+for (let i = 0; i < arrEmail.length; i++) {
+    console.log(arrEmail[i])
+    if (emailValue == arrEmail) {
+      console.log("corretto");
+    } else if (emailValue !== arrEmail) {
+      console.log("sbagliato");
     }
+  }
 
 }
 
